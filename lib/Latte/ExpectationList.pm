5563c425ef07c8aec0d3cd512a98167ddbfd8420
Logan Bell <loganbell@gmail.com>
2011-06-28 22:23:29 -0700

    Added BUILD and add

--- lib/Latte/ExpectationList.pm
+++ lib/Latte/ExpectationList.pm
@@ -1,6 +1,22 @@
 package Latte::ExpectationList;
 use Moose;
 
+sub BUILD
+{
+    my ($self) = @_;
+    
+    $self->{expectations} = [];
+
+    return $self;
+}
+
+sub add 
+{
+    my ($self, $expectation) = @_;
+    unshift @{$self->{expectations}}, $expectation;
+    return $expectation
+}
+
 __PACKAGE__->meta->make_immutable;
 no Moose;
 1;
