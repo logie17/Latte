4f244ad8f9d6cd80072be1f1cab5a916249e1fff
Logan Bell <loganbell@gmail.com>
2011-07-28 17:25:04 -0700

    Added some other ported methods.

--- lib/Latte/Expectation.pm
+++ lib/Latte/Expectation.pm
@@ -2,11 +2,21 @@ package Latte::Expectation;
 use Moose;
 use Latte::MethodMatcher;
 use Latte::ReturnValues;
+use Latte::Cardinality
 
 has mock => (
 	is  		=> 'rw',
 	isa 		=> 'Latte::Mock',
 	required 	=> 1
+);
+
+has cardinality => (
+    is          => 'rw',
+    isa         => 'Latte::Cardinality'
+)
+
+has invocation_count => (
+    is          => 'rw'
 );
 
 has method_matcher => (
@@ -22,6 +32,8 @@ sub BUILD
 {
     my ($self) = @_;
     $self->return_values(Latte::ReturnValues->new);
+    $self->cardinality(Latte::Cardinality->exactly(1)); 
+    $self->invocation_count(0);
 }
 
 sub returns
@@ -34,7 +46,7 @@ sub returns
 sub match 
 {
     my ($self, $actual_method_name, @actual_parameters) = @_;
-    $self->method_matcher->match($actual_method_name); && $self->parameters_matcher->match(actual_parameters) && $self->in_correct_order;
+    $self->method_matcher->match($actual_method_name) && $self->parameters_matcher->match(@actual_parameters) && $self->in_correct_order;
 }
 
 sub _build_method_matcher
@@ -42,6 +54,13 @@ sub _build_method_matcher
 	my ($self, $expected_method_name ) = @_;
 	return Latte::MethodMatcher( expected_method_name => $expected_method_name );
 }
+
+sub invocations_allowed
+{
+    my ($self) = @_;
+    $self->cardinality->invocations_allowed($self->invocation_count)
+}
+
 
 sub in_correct_order 
 {
