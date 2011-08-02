c888165968b680f6ce84adc90ac7e6a14d8296a5
Logan Bell <loganbell@gmail.com>
2011-08-01 23:04:17 -0700

    Added paramater_match, match, and various logic.

--- lib/Latte/ParametersMatcher.pm
+++ lib/Latte/ParametersMatcher.pm
@@ -1,13 +1,28 @@
 package Latte::ParametersMatcher;
 use Moose;
-use ExpectedParameters::AnyParameters;
+use Latte::ParametersMatcher::AnyParameters;
 
 has expected_parameters => (
     is => 'rw'
 );
 
+
 sub BUILD {
-    $self->exected_parameters(ExpectedParameters::AnyParameters->new);   
+    my ($self) = @_;
+    $self->expected_parameters(Latte::ParametersMatcher::AnyParameters->new);   
 }
+
+sub match {
+    my ( $self, @actual_parameters) = @_;
+
+    return $self->parameters_match(@actual_parameters);
+}
+
+sub parameters_match
+{
+    my ( $self, @actual_parameters) = @_;
+    return $self->expected_parameters->matches(@actual_parameters);
+}
+
 
 1;
